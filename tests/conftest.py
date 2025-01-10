c4c2c4a2ef4bfc205f9f1133411b6dfa541b6f4a
ariczhu-waymark <aric.zhu@waymarkcare.com>
2025-01-10 15:46:59 -0500

    Updating the s3 file regex to work with Tuva v0.13.0 (#5)
    The [dbt_project.yml
    ](https://raw.githubusercontent.com/tuva-health/tuva/v0.13.0/dbt_project.yml)changed
    how it organizes/loads seed names. This regex should now properly
    extract s3 and bucket names to get these seeds.
    
    ---------
    
    Co-authored-by: Sistania Bong <sistania.bong@waymarkcare.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,12 @@ import importlib
 import os
 import sys
 from io import BytesIO
-from typing import Generator
-
+from typing import Generator, Any
 import boto3
 import pytest
 from moto import mock_aws
 from mypy_boto3_s3 import S3Client
-from tuva import TUVA_SEED_FILES_0_8_6
+from tuva import TUVA_SEED_FILES_0_8_6, TUVA_SEED_FILES_0_13_0
 from typer import Typer
 from typer.testing import CliRunner
 
@@ -40,14 +39,26 @@ def target_s3_bucket() -> str:
 
 
 @pytest.fixture
-def s3(tuva_s3_bucket: str, target_s3_bucket: str) -> Generator[S3Client, None, None]:
+def s3(
+    tuva_s3_bucket: str, target_s3_bucket: str, request: Any
+) -> Generator[S3Client, None, None]:
+    version = request.param
+    print(f"Using version {version}")
+
+    if version == "0.8.6":
+        seed_files = TUVA_SEED_FILES_0_8_6  # pragma: no cover
+    elif version == "0.13.0":
+        seed_files = TUVA_SEED_FILES_0_13_0  # pragma: no cover
+    else:
+        raise ValueError(f"Unknown version {version}")  # pragma: no cover
+
     with mock_aws():
         s3: S3Client = boto3.client("s3", region_name="us-east-1")
         s3.create_bucket(Bucket=tuva_s3_bucket)
         s3.create_bucket(Bucket=target_s3_bucket)
 
-        print("Uploading mock seed data for v0.8.6")
-        for s3_key in TUVA_SEED_FILES_0_8_6:
+        print(f"Uploading mock seed data for v{version}")
+        for s3_key in seed_files:
             contents = BytesIO()
             with gzip.GzipFile(fileobj=contents, mode="w") as f:
                 # For now, just put the S3 key as the file contents
@@ -55,6 +66,6 @@ def s3(tuva_s3_bucket: str, target_s3_bucket: str) -> Generator[S3Client, None, 
             contents.seek(0)
 
             s3.upload_fileobj(contents, tuva_s3_bucket, s3_key)
-        print("...done uploading mock seed data for v0.8.6")
+            print(f"...done uploading mock seed data for v{version} {s3_key}")
 
         yield s3
